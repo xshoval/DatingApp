21ee69ae63bca3a02dbddfbf58e01678777bd0dd
xshoval <shovalsmail@gmail.com>
2024-12-29 15:59:38 +0200

    EOS 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace API.Data;
+
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+public class DataContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<AppUser> Users { get; set; }
+}
